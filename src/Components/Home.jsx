22cb6d8fb9ff925a6de35aebdb17cf31a2c9dbc5
Manya-tech <85272000+Manya-tech@users.noreply.github.com>
2023-06-26 10:17:13 +0000

    picture change

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/girl.jpg";
+import image from "../images/firstt.jpg";
 
 const imageAltText = "A girl sitting on her study table and coding using two devices.";
 
